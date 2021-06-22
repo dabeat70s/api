ee5d3162a8fee98275230561429d71f4cc8f6f96
dabeat <dabeat1@hotmail.com>
2021-06-22 19:26:22 +0100

    validation class level and commented out property level

--- CourseLibraryAPI/Models/CourseForCreationDto.cs
+++ CourseLibraryAPI/Models/CourseForCreationDto.cs
@@ -1,15 +1,31 @@
-﻿using System;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    [CourseTitleMustBeDifferentFromDescriptionAttribute 
+        (ErrorMessage ="Title & Descr must be different")]
+    public class CourseForCreationDto //:IValidatableObject // only fires if all other validations succeed
     {
+        [Required(ErrorMessage ="Title Required")]
+        [MaxLength(100,ErrorMessage ="100 Char only")]
         public string Title { get; set; }
 
+        [MaxLength(500, ErrorMessage = "500 Char only")]
         public string Description { get; set; }
 
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided description should be different from the title",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
---
+++ CourseLibraryAPI/ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -0,0 +1,33 @@
+﻿using CourseLibrary.API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.ValidationAttributes
+{
+    public class CourseTitleMustBeDifferentFromDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, 
+            ValidationContext validationContext)
+        {
+            var course = (CourseForCreationDto)validationContext.ObjectInstance;
+
+            if (course.Title == course.Description)
+            {
+                return new ValidationResult(ErrorMessage,//("Description and Title have to be different",
+                    new[] { nameof(CourseForCreationDto) });
+            }
+            //var course = (CourseForManipulationDto)validationContext.ObjectInstance;
+
+            //if (course.Title == course.Description)
+            //{
+            //    return new ValidationResult(ErrorMessage,
+            //        new[] { nameof(CourseForManipulationDto) });
+            //}
+
+            return ValidationResult.Success;
+        }
+    }
+}
